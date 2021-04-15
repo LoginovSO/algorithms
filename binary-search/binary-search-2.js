f186bb88e99fae41bd881ace46c25e4fc65d3344
Sergei <LoginovSO@yandex.ru>
2021-04-15 23:07:29 +0300

    2

---
+++ binary-search/binary-search-2.js
@@ -0,0 +1,20 @@
+// Бинарный поиск + рекурсия
+
+const arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let count = 0;
+
+function recursiveBinarySearch(array, item, start, end) {
+    let middle = Math.floor((start + end) / 2);
+    count += 1
+    if (item === array[middle]) {
+        return middle
+    }
+    if (item < array[middle]) {
+        return recursiveBinarySearch(array, item, 0, middle - 1 )
+    } else {
+        return recursiveBinarySearch(array, item, middle + 1, end )
+    }
+}
+
+console.log(recursiveBinarySearch(arr, 4, 0, arr.length));
+console.log('count = ', count);
