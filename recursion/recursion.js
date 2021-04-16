2124604bde1d3379ef26311a0cf6cb0d84525b0c
Sergei <LoginovSO@yandex.ru>
2021-04-16 18:20:12 +0300

    4

---
+++ recursion/recursion.js
@@ -0,0 +1,20 @@
+// Функция вычисления факториала
+const factorial = (n) => {
+    if (n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
+
+// Функция вычисления числа фибоначчи 1,1,2,3,5,8,13,21,34
+
+const fibonachi = (n) => {
+    if (n === 1 || n === 2) {
+        return 1;
+    }
+    return fibonachi (n - 1) + fibonachi (n - 2)
+}
+
+console.log('fibonachi = ', fibonachi(9));
---
+++ sort/hoara-sort.js
@@ -0,0 +1,11 @@
+// Сортировка хоара (Бастрая сортировка)
+
+const arr = [0,3,2,5,68,1,9,4,2,1,2,9,64,1,7,-1,-5,23,6,2,35,6,3,32];
+let count = 0;
+
+function hoaraSort(array) {
+
+}
+
+console.log('hoaraSort', hoaraSort(arr));
+console.log('count = ', count);
